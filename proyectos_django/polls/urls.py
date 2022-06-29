9b2e4214674ff02b6e5c4407f29ae9447490c304
DanielSchuler <daniel.andreas87@gmail.com>
2022-06-29 09:09:22 -0500

    cambio a las vistas de la aplicacion de votaciones

--- proyectos_django/polls/urls.py
+++ proyectos_django/polls/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 
-from .views import index,detail,results,vote
+from .views import IndexView,DetailView,ResultView,vote
 
 app_name = "polls"
 urlpatterns=[
     #Ex: /polls/
-    path("", index,name="index"),
+    path("", IndexView.as_view(),name="index"),
     #Ex: /polls/5/
-    path("<int:question_id>/", detail,name="detail"),
+    path("<int:pk>/", DetailView.as_view(),name="detail"),
     #Ex: /polls/5/results
-    path("<int:question_id>/results/", results,name="results"),
+    #antes -> path("<int:question_id>/results/", results,name="results"),
+    #ahora
+    path("<int:pk>/results/", ResultView.as_view(),name="results"),
     #Ex: /polls/5/votes
-    path("<int:question_id>/vote/", vote,name="vote")
+    path("<int:pk>/vote/", vote,name="vote")
 ]
