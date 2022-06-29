2ca6b28cb41e5743d3ae31c5966bf1be43370a58
DanielSchuler <daniel.andreas87@gmail.com>
2022-06-29 01:47:37 -0500

    commit de las vistas de votaciones

--- proyectos_django/polls/urls.py
+++ proyectos_django/polls/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from . import views
+from .views import index,detail,results,vote
 
+app_name = "polls"
 urlpatterns=[
-
-    path("", views.index,name="index")
+    #Ex: /polls/
+    path("", index,name="index"),
+    #Ex: /polls/5/
+    path("<int:question_id>/", detail,name="detail"),
+    #Ex: /polls/5/results
+    path("<int:question_id>/results/", results,name="results"),
+    #Ex: /polls/5/votes
+    path("<int:question_id>/vote/", vote,name="vote")
 ]
